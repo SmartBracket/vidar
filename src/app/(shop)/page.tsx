c25126e1de8389b5b8673c14db05d827b638a61a
SmartBracket <SmartBracket@mail.ru>
2024-02-18 16:52:33 +0300

    changed apiRouteDbQuery to functionDbQuery

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -6,21 +6,15 @@ import ShopFilters from '@/components/shop/ShopFilters'
 import ShopList from '@/components/shop/ShopList'
 import ShopPagination from '@/components/shop/ShopPagination'
 
+import { getShopProducts, getShopCats } from '@/lib/shop/queries'
+
 export default async function Home({searchParams} : any) {
-  let spObject = new URLSearchParams(searchParams);
-  let spString = spObject.toString()
-  let fetchUrl = spString.length ? `${process.env.NEXT_PUBLIC_URL}/api/shop/products?${spString}` : `${process.env.NEXT_PUBLIC_URL}/api/shop/products`
-  let fetching = await fetch(fetchUrl, {method: 'GET' , cache: 'no-store'})
-  let fetchedData = await fetching.json()
+  const productsData = await getShopProducts(searchParams)
+  const productsCount = productsData.totalCount[0].count
+  const productsList = productsData.currentProducts
 
-  let totalResulst = fetchedData[0].totalCount[0].count
-  let productsList = fetchedData[0].currentProducts
+  const catList = await getShopCats()
 
-  let fetchCatList = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/shop/cats`, {method: 'GET' , cache: 'no-store'})
-  let catList = await fetchCatList.json()
-  
-
-  // console.log(fetchedData[0].currentProducts)
   return (
     <main className="page shop">
       <div className="container">
@@ -28,7 +22,7 @@ export default async function Home({searchParams} : any) {
         <h1 className="pageTitle">Витрина аптеки</h1>
         <ShopFilters cats={catList}/>
         <ShopList products={productsList}/>
-        <ShopPagination productsCount={totalResulst}  urlParams={searchParams}/>
+        <ShopPagination productsCount={productsCount}  urlParams={searchParams}/>
         {/* <ShopVitrina /> */}
       </div>
     </main>
---
+++ src/lib/shop/queries.js
@@ -0,0 +1,57 @@
+import { connectToDatabase } from "@/core/mongo/db";
+import shopSettings from '@/lib/shop/shopSettings'
+
+const { db } = await connectToDatabase(); 
+export async function getShopProducts(pageParams){
+    let dbPage = pageParams.page ? pageParams.page : 1
+    let dbFindQuery = {}
+    let dbSortQuery = {'_id' : 1}
+
+    for(let key in pageParams){
+        switch(key){
+            case 'inStock' : 
+                dbFindQuery.inStock = true
+              break
+            case 'category' : 
+                dbFindQuery.category = pageParams[key]
+              break  
+            case 'sortBy' :
+              if(pageParams[key] == 'cheap') dbSortQuery = {'price' : 1}
+              if(pageParams[key] == 'expensive') dbSortQuery = {'price' : -1}
+              break
+            default : break
+        }
+    }
+
+    // console.log('FindQuery', dbFindQuery)
+    // console.log('SotrQuery', dbSortQuery)
+    // console.log('Page', dbPage)
+
+    const fetchResult = await db.collection("products").aggregate([
+        {
+          $facet : {
+            'totalCount' : [{$match : dbFindQuery},{  $count: 'count' }],
+            'currentProducts' : [
+              {$match : dbFindQuery},
+              {$sort : dbSortQuery},
+              { $skip: (dbPage - 1) * shopSettings.productsPerPage },
+              { $limit: 10 }
+            ]
+          }
+        }
+    ]).toArray()
+
+    const result = JSON.parse(JSON.stringify(fetchResult[0]))
+    return result
+}
+
+export async function getShopCats(){
+    const result = await db.collection("products").aggregate([
+      { 
+        $group: { _id: "$category", count: { $sum: 1 } },
+      },
+      {$sort: {'_id': 1}}
+    ]).toArray();
+    console.log(result)
+    return result
+}
